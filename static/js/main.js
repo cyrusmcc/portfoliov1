0fa9123ee7f57577ce78976132d288b7c4d4aebd
cyrusmcc <61042997+cyrusmcc@users.noreply.github.com>
2021-02-06 01:27:01 -0700

    Add skill styling and script

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+function overBack() {
+    console.log("called");
+    document.getElementById("cImg4").style.filter = "grayscale(0)";
+    document.getElementById("skills").style.opacity = 1;
+    document.getElementById("skills").style.filter = "grayscale(0)";
+}
+
+function out() {
+    document.getElementById("skills").style.opacity = 0.5;
+}
