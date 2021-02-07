a8599b2b06eaaf7a01f5d27090dd8de848fbebe1
cyrusmcc <cyrusmcawesome@hotmail.com>
2021-02-06 20:06:59 -0700

    Add animation to skill wheel

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,70 @@
 function overBack() {
-    console.log("called");
-    document.getElementById("cImg4").style.filter = "grayscale(0)";
-    document.getElementById("skills").style.opacity = 1;
-    document.getElementById("skills").style.filter = "grayscale(0)";
+
+    document.getElementById("cImgMask").style.bottom = "2.2em";
+    document.getElementById("cImgMask").style.left = "5.4em";
+
+    document.getElementById("skills").style.border = "solid thin #219915";
+
+    document.getElementById("skillsMask").style.display = "block";
+
+    document.getElementById("cImgMask").style.display = "block";
+
+    document.getElementById("cImg4").style.opacity = "1";
+    document.getElementById("cImg5").style.opacity = "1";
+    document.getElementById("cImg6").style.opacity = "1";
+    document.getElementById("cImg7").style.opacity = "1";
+    document.getElementById("cImgCont4").style.filter = "grayscale(0)";
+    document.getElementById("cImgCont5").style.filter = "grayscale(0)";
+    document.getElementById("cImgCont6").style.filter = "grayscale(0)";
+    document.getElementById("cImgCont7").style.filter = "grayscale(0)";
+
+    console.log("in");
+
+}
+
+function overFront() {
+
+    document.getElementById("skillsMask").style.display = "block";
+
+    document.getElementById("cImgMask").style.display = "block";
+
+    document.getElementById("skills").style.border = "solid thin #219915";
+
+    document.getElementById("cImgMask").style.bottom = "7em";
+    document.getElementById("cImgMask").style.left = "-5.4em";
+
+    document.getElementById("cImg1").style.opacity = "1";
+    document.getElementById("cImg2").style.opacity = "1";
+    document.getElementById("cImg3").style.opacity = "1";
+    document.getElementById("cImg8").style.opacity = "1";
+    document.getElementById("cImgCont1").style.filter = "grayscale(0)";
+    document.getElementById("cImgCont2").style.filter = "grayscale(0)";
+    document.getElementById("cImgCont3").style.filter = "grayscale(0)";
+    document.getElementById("cImgCont8").style.filter = "grayscale(0)";
+
 }
 
 function out() {
-    document.getElementById("skills").style.opacity = 0.5;
+
+    document.getElementById("skillsMask").style.display = "none";
+
+    document.getElementById("cImgMask").style.display = "none";
+
+    document.getElementById("skills").style.border = "thin solid rgba(128, 128, 128, 0.5)";
+
+    document.getElementById("cImg4").style.opacity = "0.5";
+    document.getElementById("cImg5").style.opacity = "0.5";
+    document.getElementById("cImg6").style.opacity = "0.5";
+    document.getElementById("cImg7").style.opacity = "0.5";
+    document.getElementById("cImgCont1").style.filter = "grayscale(1)";
+    document.getElementById("cImgCont2").style.filter = "grayscale(1)";
+    document.getElementById("cImgCont3").style.filter = "grayscale(1)";
+    document.getElementById("cImgCont4").style.filter = "grayscale(1)";
+    document.getElementById("cImgCont5").style.filter = "grayscale(1)";
+    document.getElementById("cImgCont6").style.filter = "grayscale(1)";
+    document.getElementById("cImgCont7").style.filter = "grayscale(1)";
+    document.getElementById("cImgCont8").style.filter = "grayscale(1)";
+
+    console.log("out");
 }
+
