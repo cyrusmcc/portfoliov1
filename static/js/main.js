652ca4990dd29a44bf4a90a0b8ea2d7f5498e899
cyrusmcc <61042997+cyrusmcc@users.noreply.github.com>
2021-02-07 02:13:35 -0700

    Add timer to skill animation

--- static/js/main.js
+++ static/js/main.js
@@ -9,14 +9,16 @@ function overBack() {
 
     document.getElementById("cImgMask").style.display = "block";
 
-    document.getElementById("cImg4").style.opacity = "1";
-    document.getElementById("cImg5").style.opacity = "1";
-    document.getElementById("cImg6").style.opacity = "1";
-    document.getElementById("cImg7").style.opacity = "1";
-    document.getElementById("cImg4").style.filter = "grayscale(0)";
-    document.getElementById("cImg5").style.filter = "grayscale(0)";
-    document.getElementById("cImg6").style.filter = "grayscale(0)";
-    document.getElementById("cImg7").style.filter = "grayscale(0)";
+    document.getElementById("skillDesc").style.display = "block";
+
+    setTimeout('document.getElementById("cImg4").style.opacity = "1";', 100);
+    setTimeout('document.getElementById("cImg5").style.opacity = "1";', 150);
+    setTimeout('document.getElementById("cImg6").style.opacity = "1";', 200);
+    setTimeout('document.getElementById("cImg7").style.opacity = "1";', 250);
+    setTimeout('document.getElementById("cImg4").style.filter = "grayscale(0)";', 100);
+    setTimeout('document.getElementById("cImg5").style.filter = "grayscale(0)";', 150);
+    setTimeout('document.getElementById("cImg6").style.filter = "grayscale(0)";', 200);
+    setTimeout('document.getElementById("cImg7").style.filter = "grayscale(0)";', 250);
 
 }
 
@@ -25,20 +27,22 @@ function overFront() {
     document.getElementById("skillsMask").style.display = "block";
 
     document.getElementById("cImgMask").style.display = "block";
+    
+    document.getElementById("skillDesc").style.display = "block";
 
     document.getElementById("skills").style.border = "solid thin #219915";
 
     document.getElementById("cImgMask").style.bottom = "7em";
     document.getElementById("cImgMask").style.left = "-5.4em";
 
-    document.getElementById("cImg1").style.opacity = "1";
-    document.getElementById("cImg2").style.opacity = "1";
-    document.getElementById("cImg3").style.opacity = "1";
-    document.getElementById("cImg8").style.opacity = "1";
-    document.getElementById("cImg1").style.filter = "grayscale(0)";
-    document.getElementById("cImg2").style.filter = "grayscale(0)";
-    document.getElementById("cImg3").style.filter = "grayscale(0)";
-    document.getElementById("cImg8").style.filter = "grayscale(0)";
+    setTimeout('document.getElementById("cImg8").style.opacity = "1";', 100);
+    setTimeout('document.getElementById("cImg1").style.opacity = "1";', 150);
+    setTimeout('document.getElementById("cImg2").style.opacity = "1";', 200);
+    setTimeout('document.getElementById("cImg3").style.opacity = "1";', 250);
+    setTimeout('document.getElementById("cImg8").style.filter = "grayscale(0)";', 100);
+    setTimeout('document.getElementById("cImg1").style.filter = "grayscale(0)";', 150);
+    setTimeout('document.getElementById("cImg2").style.filter = "grayscale(0)";', 200);
+    setTimeout('document.getElementById("cImg3").style.filter = "grayscale(0)";', 250);
 
 }
 
@@ -47,6 +51,8 @@ function out() {
     document.getElementById("skillsMask").style.display = "none";
 
     document.getElementById("cImgMask").style.display = "none";
+    
+    document.getElementById("skillDesc").style.display = "none";
 
     document.getElementById("skills").style.border = "thin solid rgba(128, 128, 128, 0.5)";
 
