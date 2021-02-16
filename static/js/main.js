b5793b4ef94a420fe0a4f484233a1d633541b667
cyrusmcc <cyrusmcc@hotmail.com>
2021-02-15 20:32:23 -0700

    Refactor third slide

--- static/js/main.js
+++ static/js/main.js
@@ -2,89 +2,6 @@ var p1Counter = 0;
 var p2Counter = 0;
 var p3Counter = 0;
 
-
-function overBack() {
-
-    document.getElementById("cImgMask").style.bottom = "0.2em";
-    document.getElementById("cImgMask").style.left = "5.4em";
-
-    document.getElementById("skills").style.border = "solid 2px #f9f3e9";
-
-    document.getElementById("skillsMask").style.display = "block";
-
-    document.getElementById("cImgMask").style.display = "block";
-
-    document.getElementById("s2Desc").style.display = "block";
-
-    document.getElementById("s2Desc").innerText = "Java, Spring, MySQL, Hibernate";
-
-    setTimeout('document.getElementById("cImg4").style.opacity = "1";', 50);
-    setTimeout('document.getElementById("cImg5").style.opacity = "1";', 100);
-    setTimeout('document.getElementById("cImg6").style.opacity = "1";', 150);
-    setTimeout('document.getElementById("cImg7").style.opacity = "1";', 200);
-    setTimeout('document.getElementById("cImg4").style.filter = "grayscale(0)";', 50);
-    setTimeout('document.getElementById("cImg5").style.filter = "grayscale(0)";', 100);
-    setTimeout('document.getElementById("cImg6").style.filter = "grayscale(0)";', 150);
-    setTimeout('document.getElementById("cImg7").style.filter = "grayscale(0)";', 200);
-
-}
-
-function overFront() {
-
-    document.getElementById("skillsMask").style.display = "block";
-
-    document.getElementById("cImgMask").style.display = "block";
-    
-    document.getElementById("s2Desc").style.display = "block";
-
-    document.getElementById("skills").style.border = "solid 2px #f9f3e9";
-
-    document.getElementById("cImgMask").style.bottom = "5em";
-    document.getElementById("cImgMask").style.left = "-5.4em";
-
-    document.getElementById("s2Desc").innerText = "HTML, CSS, SASS, JavaScript";
-
-
-    setTimeout('document.getElementById("cImg8").style.opacity = "1";', 50);
-    setTimeout('document.getElementById("cImg1").style.opacity = "1";', 100);
-    setTimeout('document.getElementById("cImg2").style.opacity = "1";', 150);
-    setTimeout('document.getElementById("cImg3").style.opacity = "1";', 200);
-    setTimeout('document.getElementById("cImg8").style.filter = "grayscale(0)";', 50);
-    setTimeout('document.getElementById("cImg1").style.filter = "grayscale(0)";', 100);
-    setTimeout('document.getElementById("cImg2").style.filter = "grayscale(0)";', 150);
-    setTimeout('document.getElementById("cImg3").style.filter = "grayscale(0)";', 200);
-
-}
-
-function out() {
-
-    document.getElementById("skillsMask").style.display = "none";
-
-    document.getElementById("cImgMask").style.display = "none";
-    
-    document.getElementById("s2Desc").style.display = "none";
-
-    document.getElementById("skills").style.border = "2px solid rgba(128, 128, 128, 0.5)";
-
-    document.getElementById("cImg1").style.opacity = "0.5";
-    document.getElementById("cImg2").style.opacity = "0.5";
-    document.getElementById("cImg3").style.opacity = "0.5";
-    document.getElementById("cImg4").style.opacity = "0.5";
-    document.getElementById("cImg5").style.opacity = "0.5";
-    document.getElementById("cImg6").style.opacity = "0.5";
-    document.getElementById("cImg7").style.opacity = "0.5";
-    document.getElementById("cImg8").style.opacity = "0.5";
-    document.getElementById("cImg1").style.filter = "grayscale(1)";
-    document.getElementById("cImg2").style.filter = "grayscale(1)";
-    document.getElementById("cImg3").style.filter = "grayscale(1)";
-    document.getElementById("cImg4").style.filter = "grayscale(1)";
-    document.getElementById("cImg5").style.filter = "grayscale(1)";
-    document.getElementById("cImg6").style.filter = "grayscale(1)";
-    document.getElementById("cImg7").style.filter = "grayscale(1)";
-    document.getElementById("cImg8").style.filter = "grayscale(1)";
-
-}
-
 function onClickProject(project, projectText, projectImg) {
     var projectId = project.id;
     var projectTextId = projectText.id;
@@ -104,7 +21,7 @@ function onClickProject(project, projectText, projectImg) {
        
         } else if(p1Counter == 1){
             
-            document.getElementById(projectId).style.height = "15em";
+            document.getElementById(projectId).style.height = "45vh";
             
             document.getElementById(projectTextId).style.webkitMaskImage = "linear-gradient(to bottom, black 48%, transparent 96%)";
            
@@ -128,7 +45,7 @@ function onClickProject(project, projectText, projectImg) {
        
         } else if(p2Counter == 1){
            
-            document.getElementById(projectId).style.height = "15em";
+            document.getElementById(projectId).style.height = "45vh";
 
             document.getElementById(projectImg).style.opacity = "0.5";
 
@@ -153,7 +70,7 @@ function onClickProject(project, projectText, projectImg) {
        
         } else if(p3Counter == 1){
            
-            document.getElementById(projectId).style.height = "15em";
+            document.getElementById(projectId).style.height = "45vh";
 
             document.getElementById(projectImg).style.opacity = "0.5em";
            
